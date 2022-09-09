997fc1d684f7ee7231ee6be69d04bfb6ff65cf11
imgitem <it@img-srl.com>
2022-09-09 16:28:29 +0200

    Commit after rebuild ven 9 set 2022, 16:28:29, CEST - - definito srumento generico con configurazione multipli canali

--- src/hielen3/feature.py
+++ src/hielen3/feature.py
@@ -32,18 +32,6 @@ class StringTime(fields.DateTime):
     def _serialize(self, value, attr, obj, **kwargs):
         return _agoodtime(value)
 
-class Feature(HFeature):
- 
-    '''
-    Default Feature
-    '''
- 
-    def setup(self,*args,**kwargs):
-        pass
- 
-    def config(self,*args,**kwargs):
-        pass
-
 
 class HFeature(ABC):
 
