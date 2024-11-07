7c162bf9ba79b3335b905ff95ae473e48cef86b7
imgitem <it@img-srl.com>
2024-11-07 15:14:21 +0100

    Commit after rebuild gio 7 nov 2024, 15:14:21, CET - ### 7 Novembre 2024 ###

--- src/hielen3/__init__.py
+++ src/hielen3/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 __name__ = 'hielen3'
-__version__ = '3.5.0'
+__version__ = '3.5.1'
 __author__ = 'Alessandro Modesti'
 __email__ = 'it@img-srl.com'
 __description__ = 'Multidimention Hierarichical Elaboration Engine'
